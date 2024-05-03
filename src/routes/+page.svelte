e811a0560cfaa44d4c6d3b8282d75416326e9d4e
Giacomo Folli <119410424+giacomo-folli@users.noreply.github.com>
2024-05-03 17:02:51 +0200

    homepage content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,39 +6,104 @@
 		class="absolute top-0 left-0 h-full w-full z-[-1] bg-cover bg-no-repeat filter sepia contrast-50"
 		style:background-image="url(/homepage.jpg)"
 	/>
-	<div id="slogan" class="mx-auto p-1 py-14">
-		<div class="text-sm text-pretty px-6">
-			A group of humans seeking to learn, experience, explore, live & laugh via a physical movement
-			practice.
+
+	<div class="lg:grid lg:grid-cols-[50%_50%]">
+		<div id="slogan" class="mx-auto p-1 py-14 lg:p-10 lg:mt-8">
+			<div class="text-sm lg:text-[1vw] px-6">
+				A group of humans seeking to learn, experience, explore, live & laugh via a physical
+				movement practice.
+			</div>
+
+			<div
+				class="mx-auto text-[20vw] lg:text-[10vw] font-semibold leading-none whitespace-pre-wrap px-6"
+			>
+				LONDON
+			</div>
+			<div
+				class="mx-auto pt-2 text-[13vw] lg:text-[7vw] tracking-widest lg:tracking-wider leading-[10vw] font-semibold whitespace-pre-wrap px-6"
+			>
+				MOVEMENT
+			</div>
+			<div
+				class="mx-auto text-[26vw] lg:text-[13vw] -tracking-wider leading-none font-semibold whitespace-pre-wrap px-6"
+			>
+				GROUP
+			</div>
+
+			<div class="w-full px-6 text-sm lg:text-[2vw] tracking-normal italic">
+				Inclusive. Connected. Supportive. Learning. Alive.
+			</div>
 		</div>
 
-		<div class="mx-auto text-7xl font-semibold whitespace-pre-wrap px-6">LONDON</div>
-		<div class="mx-auto pt-2 text-5xl tracking-widest font-semibold whitespace-pre-wrap px-6">
-			MOVEMENT
-		</div>
-		<div class="mx-auto text-8xl -tracking-wider trackin font-semibold whitespace-pre-wrap px-6">
-			GROUP
-		</div>
-
-		<div class="w-full px-6 text-sm text-justify">
-			Inclusive. Connected. Supportive. Learning. Alive.
-		</div>
-	</div>
-
-	<div class="mt-8 flex justify-end">
-		<div class="w-fit px-4">
-			<img src="/logo.png" alt="log" height="200" width="200" />
-			<div class="py-6 underline underline-offset-4 text-xl tracking-wider">
-				book a class now &rarr;
+		<div class="mt-8 flex justify-end lg:p-16">
+			<div class="px-4 h-[200px] w-[200px] lg:h-[400px] lg:w-[400px]">
+				<img src="/logo.png" alt="log" />
+				<div
+					class="py-3 underline underline-offset-4 text-[5vw] lg:text-lg tracking-wide lg:tracking-wider lg:text-right"
+				>
+					<a href="/classi"> book a class now &rarr; </a>
+				</div>
 			</div>
 		</div>
 	</div>
 </section>
 
-<section class="p-4 border-[18px] border-black">
-	<div class="grid w-fit mx-8 text-justify">
-		<div class="clamped text-7xl">We are</div>
-		<div class="clamped text-5xl tracking-wide">a diverse</div>
-		<div class="clamped text-2xl tracking-widest font-semibold">group of humans</div>
+<section class="relative px-4 border-[18px] border-black">
+	<div class="absolute top-2 left-2 font-semibold">*Manifesto</div>
+
+	<div class="w-full text-center mt-10 mb-32 lg:mt-20 lg:mb-64 leading-[16vw]">
+		<div class="text-[25vw] leading-[19vw]">We are</div>
+		<div class="text-[19vw]">a diverse</div>
+		<div class="text-[10vw] tracking-tight leading-none text-amber-600 filter sepia-[50%]">group of humans</div>
+	</div>
+
+	<div class="ml-[30vw] lg:ml-[70vw] mb-24 lg:mb-48 leading-[8vw]">
+		<div class="text-[6vw] lg:text-3xl">engaging in many</div>
+		<div class="text-[6vw] lg:text-3xl tracking-tight italic">types of movement,</div>
+	</div>
+
+	<div class="leading-[7vw] tracking-tighter mb-24 lg:mb-64">
+		<div class="text-[7vw] lg:text-5xl">with the intention of</div>
+		<div class="text-[7vw] lg:text-5xl font-semibold">learning more about</div>
+		<div class="text-[15vw] leading-[10vw] italic">ourselves,</div>
+	</div>
+
+	<div class="leading-[11vw] mb-32 lg:mb-64">
+		<div class="text-[12vw] flex gap-2 lg:gap-8">
+			both <div class="italic underline underline-offset-8 mt-4 lg:mt-8 text-amber-600 filter sepia-[50%]">physically</div>
+		</div>
+		<div class="text-[13vw] font-[500] flex gap-4 lg:gap-16	">
+			and <div class="italic underline underline-offset-8 mt-2 lg:mt-6 text-amber-600 filter sepia-[50%]">mentally</div>
+		</div>
+	</div>
+
+	<div class="mb-16 lg:mb-32">
+		<div class="text-[5vw] tracking-tight leading-none italic">
+			We are dancers and photographers,
+		</div>
+		<div class="text-[5vw] tracking-wide leading-none text-right font-mono">accountants and managers,</div>
+		<div class="text-[5vw] leading-none italic font-serif">architects and builders alike.</div>
+	</div>
+
+	<div class="leading-[10vw] mb-8 lg:mb-20">
+		<div class="text-[10vw] tracking-tighter">we are here to learn</div>
+		<div class="text-[13vw] tracking-tighter flex gap-3 lg:gap-8 items-baseline">and
+			<div class="lg:text-[16vw] italic text-amber-600 filter sepia-[50%]">
+				experience.
+			</div>
+			</div>
+	</div>
+</section>
+
+<section class="relative h-96 pt-8 pb-14">
+	<div
+		class="absolute bg-center top-0 left-0 h-full w-full z-[-1] bg-cover bg-no-repeat filter sepia contrast-50"
+		style:background-image="url(/homepage.jpg)"
+	/>
+
+	<div
+		class="absolute bottom-4 right-4 p-3  filter sepia font-bold shadow-lg border-4 border-black backdrop-blur-sm"
+	>
+		<button> Unisciti a noi</button>
 	</div>
 </section>
--- src/routes/personal/+page.svelte
+++ src/routes/personal/+page.svelte
@@ -1,16 +1,13 @@
-<div class="w-full text-center text-3xl">
-    Personal
-</div>
-
-<main class="p-4 text-slate-300">
+<div class="w-full pt-20 pb-6 text-center text-3xl">Personal</div>
+<main class="p-4 pt-2 h-[40vh] text-slate-300">
 	<svg height="200px" width="100%">
-		<line x1=0 y1=4 x2=400 y2=4/>
-		<line x1=0 y1=34 x2=200 y2=34/>
-		<line x1=0 y1=64 x2=300 y2=64/>
-		<line x1=0 y1=94 x2=280 y2=94/>
-        <line x1=0 y1=124 x2=200 y2=124/>
-		<line x1=0 y1=154 x2=300 y2=154/>
-		<line x1=0 y1=184 x2=280 y2=184/>
+		<line x1="0" y1="4" x2="400" y2="4" />
+		<line x1="0" y1="34" x2="200" y2="34" />
+		<line x1="0" y1="64" x2="300" y2="64" />
+		<line x1="0" y1="94" x2="280" y2="94" />
+		<line x1="0" y1="124" x2="200" y2="124" />
+		<line x1="0" y1="154" x2="300" y2="154" />
+		<line x1="0" y1="184" x2="280" y2="184" />
 	</svg>
 </main>
 
