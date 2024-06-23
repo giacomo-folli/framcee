f4ce80aa301b166c2bd1ab75407376f022d21603
Giacomo Folli <119410424+giacomo-folli@users.noreply.github.com>
2024-06-23 20:00:28 +0200

    start on servizi page

--- src/routes/servizi/+page.svelte
+++ src/routes/servizi/+page.svelte
@@ -1,65 +1,174 @@
-<script lang="ts"></script>
+<script lang="ts">
+	import FeatureIcon from '$lib/components/Icons/FeatureIcon.svelte';
+</script>
 
-<section class="py-14 pt-20 md:py-24 bg-white dark:bg-[#0b1727] text-zinc-900 dark:text-white">
-	<div class="w-full px-10 md:px-8 lg:px-28">
-		<div class="grid grid-cols-12 mb-6">
-			<div class="col-span-12 lg:col-span-8 lg:col-start-3 lg:px-14 text-center">
-				<p class="text-base leading-6">
-					Dominion dry make shall is is greater fish moving set seas open man which. Green make
-					appear moveth fruit likeness.
+<div class="h-[100vh] px-[25px] flex flex-col justify-center">
+	<p class="font-bold text-5xl mb-4">Programmi completi e per tutti</p>
+	<p class="text-xl">
+		Offro diversi tipi di allenamento, sempre pensati per soddisfare al meglio i bisogni
+		individuali.
+	</p>
+</div>
+
+<section id="section-3" class="relative px-[25px] py-14 md:py-24">
+	<div class="flex items-center gap-8">
+		<div class="basis-1/2 flex justify-center items-center">
+			<img
+				src="/homepage.jpg"
+				alt="foto"
+				class="h-[500px] w-[600px] object-cover rounded-sm shadow-lg"
+			/>
+		</div>
+		<div class="basis-1/2 h-full flex flex-col gap-16 justify-between items-start">
+			<div>
+				<p class="font-bold text-4xl mb-4">Allenamento di Mobilità</p>
+				<p class="text-md">
+					Migliora il tuo range di movimento e la tua flessibilità con un programma specializzato.
+				</p>
+			</div>
+			<div>
+				<p class="font-bold text-4xl mb-4">Allenamento di Forza</p>
+				<p class="text-md">
+					Migliora il tuo range di movimento e la tua flessibilità con un programma specializzato.
+				</p>
+			</div>
+			<div>
+				<p class="font-bold text-4xl mb-4">Sessioni di Movimento</p>
+				<p class="text-md">
+					Sessioni con un gruppo di atleti, pensate per affrontare il movimento sotto diversi
+					aspetti.
 				</p>
 			</div>
 		</div>
+	</div>
+</section>
 
-		<div class="mx-auto max-md:max-w-md grid grid-cols-12 gap-6">
-			<div class="col-span-12 md:col-span-4">
-				<div class="rounded-[20px] bg-blue-50 dark:bg-[#1E2735] relative overflow-hidden">
-					<div
-						class="w-[85px] h-[92px] bg-[#fc6e5c] text-white rounded-br-[42px] text-[35px] inline-flex justify-center items-center mb-4"
-					>
-						<i class="fas fa-user"></i>
+<section id="section-3" class="relative px-[25px] py-14 md:pb-24">
+	<div class="flex items-center justify-center">
+		<div class="basis-1/2 w-full flex flex-col gap-4 justify-center max-w-lg">
+			<div class="mb-2">
+				<p class="font-semibold text-sm">Trasformazione</p>
+				<p class="font-bold text-4xl mb-4">Allenamento di Mobilità</p>
+				<p class="p-normal">
+					Experience a holistic approach to training that focuses on developing your mind and body.
+					With Francesco, you'll improve your mobility, strength, flexibility, and coordination,
+					leading to overall growth and well-being.
+				</p>
+			</div>
+
+			<div class="flex">
+				<div>
+					<h5>Approccio olistico</h5>
+					<p class="p-normal">
+						Ricevi un'attenzione e una guida personalizzate per raggiungere i tuoi obiettivi di
+						fitness in modo efficace.
+					</p>
+				</div>
+				<div>
+					<h5>Programmi personalizzati</h5>
+					<p class="p-normal">
+						Sviluppa la tua mente e il tuo corpo attraverso programmi di allenamento personalizzati
+						in base alle tue esigenze.
+					</p>
+				</div>
+			</div>
+		</div>
+		<div class="basis-1/2 flex justify-center items-center">
+			<img
+				src="/homepage.jpg"
+				alt="foto"
+				class="h-[500px] w-[600px] object-cover rounded-sm shadow-lg"
+			/>
+		</div>
+	</div>
+</section>
+
+<section id="section-3" class="relative px-[25px] py-14 md:pb-24">
+	<div class="flex items-center justify-center">
+		<div class="basis-1/2 w-full flex flex-col gap-4 justify-center max-w-lg">
+			<div class="mb-2">
+				<p class="font-semibold text-sm">Trasformazione</p>
+				<p class="font-bold text-4xl mb-4">Allenamento di Mobilità</p>
+				<p class="p-normal">
+					Experience a holistic approach to training that focuses on developing your mind and body.
+					With Francesco, you'll improve your mobility, strength, flexibility, and coordination,
+					leading to overall growth and well-being.
+				</p>
+			</div>
+
+			<div class="flex">
+				<div>
+					<h5>Approccio olistico</h5>
+					<p class="p-normal">
+						Ricevi un'attenzione e una guida personalizzate per raggiungere i tuoi obiettivi di
+						fitness in modo efficace.
+					</p>
+				</div>
+				<div>
+					<h5>Programmi personalizzati</h5>
+					<p class="p-normal">
+						Sviluppa la tua mente e il tuo corpo attraverso programmi di allenamento personalizzati
+						in base alle tue esigenze.
+					</p>
+				</div>
+			</div>
+		</div>
+		<div class="basis-1/2 flex justify-center items-center">
+			<img
+				src="/homepage.jpg"
+				alt="foto"
+				class="h-[500px] w-[600px] object-cover rounded-sm shadow-lg"
+			/>
+		</div>
+	</div>
+</section>
+
+<section id="section-3" class="relative px-[25px] py-14 md:py-24">
+	<div class="flex flex-col items-center justify-center">
+		<div class="w-full flex gap-4 justify-start">
+			<div class="basis-1/2 mb-2">
+				<p class="font-bold text-4xl mb-2">Allenamento di Mobilità</p>
+				<p class="font-semibold text-sm">Trasformazione</p>
+			</div>
+
+			<div class="basis-1/2 flex flex-col gap-6">
+				<div>
+					<p class="p-normal">
+						Experience a holistic approach to training that focuses on developing your mind and
+						body. With Francesco, you'll improve your mobility, strength, flexibility, and
+						coordination, leading to overall growth and well-being.
+					</p>
+				</div>
+				<div class="flex gap-2">
+					<div>
+						<div class="my-6">
+							<FeatureIcon />
+						</div>
+						<h5>Approccio olistico</h5>
+						<p class="text-sm">
+							Ricevi un'attenzione e una guida personalizzate per raggiungere i tuoi obiettivi di
+							fitness in modo efficace.
+						</p>
 					</div>
-					<div class="p-6 lg:px-12 lg:pb-12">
-						<h4 class="text-2xl font-bold mb-4">1:1 Coaching</h4>
-						<p class="opacity-70">
-							Assumenda non repellendus distinctio nihil dicta sapiente, quibusdam maiores, illum
-							at, aliquid blanditiis eligendi qui.
+					<div>
+						<div class="my-6">
+							<FeatureIcon />
+						</div>
+						<h5>Programmi personalizzati</h5>
+						<p class="text-sm">
+							Sviluppa la tua mente e il tuo corpo attraverso programmi di allenamento
+							personalizzati in base alle tue esigenze.
 						</p>
 					</div>
 				</div>
 			</div>
-			<div class="col-span-12 md:col-span-4">
-				<div class="rounded-[20px] bg-blue-50 dark:bg-[#1E2735] relative overflow-hidden">
-					<div
-						class="w-[85px] h-[92px] bg-[#0479c1] text-white rounded-br-[42px] text-[35px] inline-flex justify-center items-center mb-4"
-					>
-						<i class="fas fa-people-group"></i>
-					</div>
-					<div class="p-6 lg:px-12 lg:pb-12">
-						<h4 class="text-2xl font-bold mb-4">Gruppi</h4>
-						<p class="opacity-70">
-							Assumenda non repellendus distinctio nihil dicta sapiente, quibusdam maiores, illum
-							at, aliquid blanditiis eligendi qui.
-						</p>
-					</div>
-				</div>
-			</div>
-			<div class="col-span-12 md:col-span-4">
-				<div class="rounded-[20px] bg-blue-50 dark:bg-[#1E2735] relative overflow-hidden">
-					<div
-						class="w-[85px] h-[92px] bg-[#96ff00] text-white rounded-br-[42px] text-[35px] inline-flex justify-center items-center mb-4"
-					>
-						<i class="fas fa-leaf"></i>
-					</div>
-					<div class="p-6 lg:px-12 lg:pb-12">
-						<h4 class="text-2xl font-bold mb-4">Movement</h4>
-						<p class="opacity-70">
-							Assumenda non repellendus distinctio nihil dicta sapiente, quibusdam maiores, illum
-							at, aliquid blanditiis eligendi qui.
-						</p>
-					</div>
-				</div>
-			</div>
+		</div>
+		<div class="w-full mt-32">
+			<img
+				src="/homepage.jpg"
+				alt="foto"
+				class="h-[500px] w-full object-cover rounded-sm shadow-lg"
+			/>
 		</div>
 	</div>
 </section>
