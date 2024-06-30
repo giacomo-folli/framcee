d5366b194f7deed7bd02627a87dd5f9130a94d2b
Giacomo Folli <119410424+giacomo-folli@users.noreply.github.com>
2024-06-30 12:08:36 +0200

    add marquee

---
+++ src/lib/components/common/Marquee.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	export let reverse: boolean = false,
+		pauseOnHover: boolean = false,
+		vertical: boolean = false,
+		repeat: number = 4;
+
+	let clazz = '';
+	export { clazz as class };
+
+	
+</script>
+
+<!-- 
+  @component
+  An infinite scrolling component that can be used to display text, images, or videos.
+-->
+
+<div
+	class={`group flex overflow-hidden p-2 [--duration:40s] [--gap:1rem] [gap:var(--gap)] ${vertical ? 'flex-col' : 'flex-row'} ${clazz}`}
+>
+	{#each { length: repeat } as _, i}
+		<div
+			class={`flex shrink-0 justify-around [gap:var(--gap)] ${vertical ? 'animate-marquee-vertical flex-col' : 'animate-marquee flex-row'} 
+            ${pauseOnHover && 'group-hover:[animation-play-state:paused]'} ${reverse && '[animation-direction:reverse]'}`}
+		>
+			<slot />
+		</div>
+	{/each}
+</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,30 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+import daisyui from 'daisyui';
+import { fontFamily } from 'tailwindcss/defaultTheme';
 
+/** @type {import('tailwindcss').Config} */
+const config = {
+	darkMode: ['class'],
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	safelist: ['dark'],
+	theme: {
+		extend: {
+			animation: {
+				marquee: 'marquee var(--duration) linear infinite',
+				'marquee-vertical': 'marquee-vertical var(--duration) linear infinite'
+			},
+			keyframes: {
+				marquee: {
+					from: { transform: 'translateX(0)' },
+					to: { transform: 'translateX(calc(-100% - var(--gap)))' }
+				},
+				'marquee-vertical': {
+					from: { transform: 'translateY(0)' },
+					to: { transform: 'translateY(calc(-100% - var(--gap)))' }
+				}
+			}
+		}
+	},
+	plugins: [daisyui]
+};
+
+export default config;
